762bf39a6a1152302115b0f437a12c467f0115d9
shadowfighterCA <advait.chandorkar@quoppo.com>
2023-05-03 16:04:54 +0530

    Revert "Added API docs"
    This reverts commit 3435c8dc559d531a41841a928978232b30b66df2.

--- MQTT_OTA/.pio/build/seeed_xiao_esp32c3/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/seeed_xiao_esp32c3/FrameworkArduino/USBMSC.cpp.o: \
- /home/advait/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- /home/advait/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /home/advait/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h
--- MQTT_OTA/.pio/build/seeed_xiao_esp32c3/FrameworkArduino/esp32-hal-tinyusb.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/seeed_xiao_esp32c3/FrameworkArduino/esp32-hal-tinyusb.c.o: \
- /home/advait/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-tinyusb.c \
- /home/advait/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h
