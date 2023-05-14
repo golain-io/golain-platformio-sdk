c9205122ba081f2902f0c473c1b99a6325b337f4
shadowfighterCA <advait.chandorkar@quoppo.com>
2023-05-14 19:41:40 +0530

    Updated library

--- Persistent_logs/include/persistent_logs.h
+++ Persistent_logs/include/persistent_logs.h
@@ -17,15 +17,18 @@
 void write_p_log(String tag, String log, String func, Level level);
 void write_to_nvs(uint8_t *buffer, size_t len);
 void read_old_logs();
-#define P_LOG_I(tag, log)                                                                         \    
+#define P_LOG_I(tag, log)                                                                         \
+    \    
 Serial.println(String("INFO_LOG -> ") + String(tag) + "  (" + __func__ + ")  -> " + String(log)); \
     write_p_log(tag, log, String(__func__), Level_INFO);
 
-#define P_LOG_W(tag, log)                                                                          \                          
+#define P_LOG_W(tag, log)                                                                          \
+    \                          
  Serial.println(String("WARN_LOG -> ") + String(tag) + "  (" + __func__ + ")  -> " + String(log)); \
     write_p_log(tag, log, String(__func__), Level_WARNING);
 
-#define P_LOG_E(tag, log)                                                                          \                        
+#define P_LOG_E(tag, log)                                                                          \
+    \                        
   Serial.println(String("ERR_LOG -> ") + String(tag) + "  (" + __func__ + ")  -> " + String(log)); \
     write_p_log(tag, log, String(__func__), Level_ERROR);
 
@@ -131,7 +134,13 @@ void read_old_logs()
         /*
         do whatever you want with the logs
         */
-      
+        Serial.printf("giving log id %d\n", last_log_id);
+        // do what you want with logs
+        for (int i = 0; i < len; i++)
+        {
+            Serial.print((char)buffer[i]);
+        }
+        Serial.println();
 
         memset(buffer, 0, 256); // clear the buffer
         last_log_id--;
